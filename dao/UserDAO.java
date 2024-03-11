2b41ee7e5b9fdec893bb7e4b46afe5adb1c3875a
yosyu218 <yosyu218@student.otago.ac.nz>
2024-03-12 11:36:55 +1300

    Potentially we can use these User domain classes

---
+++ dao/UserDAO.java
@@ -0,0 +1,15 @@
+package dao;
+
+import domain.User;
+/*Yuki Yoshiyasu */ 
+public interface UserDAO{
+
+    void saveUser(User user);
+
+    void removeUser(User user);
+
+    User getUserByUsername(String username);
+
+    boolean verifyCredentials(String username, String password);
+    
+}
